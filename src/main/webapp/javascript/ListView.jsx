ac14849bab46331c70af9dbf84283b00a81754fc
fyhao <fyhao1@gmail.com>
2021-07-28 14:42:45 +0800

    #171 init react

---
+++ src/main/webapp/javascript/ListView.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Table,Button } from 'reactstrap';
+import ee from './EventManager';
+class ListView extends Component {
+  render() {
+	 const options = this.props.options;
+	 const fields = options.fields;
+	 const data = options.data;
+    return (
+      <div>
+		<Table>
+			<thead><tr>
+		{fields.map((field,i) => {
+			return (<th key={i}>{field.heading}</th>)
+		})}
+			{options.hasEdit && <th>Edit</th>}
+		    {options.hasDelete && <th>Delete</th>}
+			</tr></thead>
+			<tbody>
+		{data.map((row,i) => {
+	      var rowItems = [];
+		  fields.map((field,i) => {
+			  var v = row[field.key];
+			  if(field.render) {
+				  v = field.render(v,row);
+			  }
+			  rowItems.push(v)
+		  })
+		  return (<tr key={i}>
+		  {rowItems.map((col,j) => {
+			  return (<td key={j}>{col}</td>)
+		  })}
+		  
+		  {options.hasEdit && <td><Button onClick={this.onEdit(row)} outline color="primary">Edit</Button></td>}
+		  {options.hasDelete && <td><Button onClick={this.onDelete(row)} outline color="danger">Delete</Button></td>}
+		  </tr>)
+		})}
+		</tbody>
+		</Table>
+	  
+	  </div>
+    );
+  }
+  
+  onEdit(row) {
+	  const options = this.props.options;
+	  return () => {
+		  ee.emit('navigatePage', {page:options.editForm(row),row:row});
+	  };
+  }
+  onDelete(row) {
+	  const options = this.props.options;
+	  return () => {
+		  options.handleDelete(row);
+	  };
+  }
+}
+
+export default ListView;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    devtool: 'source-map',
+    output: {
+        filename: 'react-app.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        },{
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    }
+};
