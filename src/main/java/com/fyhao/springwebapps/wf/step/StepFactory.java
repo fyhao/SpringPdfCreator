8f823e4bb199a77eb75c98580dfacaf040fc418c
fyhao <fyhao1@gmail.com>
2021-07-30 22:08:00 +0800

    #184 StepFactory use ClassPathScanningCandidateComponentProvider

--- src/main/java/com/fyhao/springwebapps/wf/step/StepFactory.java
+++ src/main/java/com/fyhao/springwebapps/wf/step/StepFactory.java
@@ -9,6 +9,12 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 
 import com.fyhao.springwebapps.wf.WFStep;
 
@@ -19,24 +25,30 @@ public class StepFactory {
 	static Map<String, Class<?>> stepClasses = new HashMap<String, Class<?>>();
 	
 	static {
-		try {
-			System.out.println("StepFactory static init " + PACKAGE_NAME);
-			Class[] classes = getClasses(PACKAGE_NAME);
-			System.out.println("StepFactory init len: " + classes.length);
-			for(Class clazz : classes) {
-				System.out.println("StepFactory init clazz: " + clazz.getCanonicalName());
+		// create scanner and disable default filters (that is the 'false' argument)
+		final ClassPathScanningCandidateComponentProvider provider = new ClassPathScanningCandidateComponentProvider(false);
+		// add include filters which matches all the classes (or use your own)
+		provider.addIncludeFilter(new RegexPatternTypeFilter(Pattern.compile(".*")));
+
+		// get matching classes defined in the package
+		final Set<BeanDefinition> classes = provider.findCandidateComponents(PACKAGE_NAME);
+
+		// this is how you can load the class type from BeanDefinition instance
+		for (BeanDefinition bean: classes) {
+		    Class<?> clazz;
+			try {
+				clazz = Class.forName(bean.getBeanClassName());
 				if(clazz.getSuperclass().equals(WFStep.class)) {
 					String stepName = deriveStepName(clazz.getCanonicalName());
 					stepClasses.put(stepName, clazz);
 				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		    
 		}
+		
 	}
 	
 	public static String deriveStepName(String clazzName) {
